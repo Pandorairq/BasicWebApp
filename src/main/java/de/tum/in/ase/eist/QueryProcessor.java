2404ad5b92983adc7ee4cd525bc887d051c1b1fa
Stefan <flummipvp@gmail.com>
2022-07-14 17:47:22 +0200

    My name is Pandorai

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -15,15 +15,17 @@ public class QueryProcessor {
            return "Pandorairq";
         }
         else if (query.contains("what is")) {
-            int i = query.indexOf("is%20");
-            String str = query.substring(i + 5,i + 7);
-            String str2 = query.substring(i + 8,i + 10);
-            int a = Integer.parseInt(str);
-            int b = Integer.parseInt(str2);
-            return Integer.toString((a + b));
+            String[] numbers = query.split(" ");
+            int x = Integer.parseInt(numbers[3]);
+            int y = Integer.parseInt(numbers[5]);
+            return x + y + "";
         }
         else if(query.contains("which of the following numbers is the largest")) {
-            String[] strings = query.split(",%20");
+            String[] strings = query.split("%20");
+            int result = 0;
+            for( int i = 9; i < strings.length; i++) {
+
+            }
             int i = query.indexOf(":");
             String str = query.substring(i + 5,i + 7);
             String str2 = query.substring(i + 8,i + 10);
